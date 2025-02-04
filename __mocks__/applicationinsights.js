4195cbefc442deb5fee743fd83cf7bf085da20fd
Matt O'Donoghue <mattyod@gmail.com>
2025-02-04 08:22:44 +0000

    test(NO-JIRA): reset mocks globally

--- __mocks__/applicationinsights.js
+++ __mocks__/applicationinsights.js
@@ -1,6 +1,10 @@
-export const appInsights = {
+const appInsights = {
   defaultClient: {
     trackEvent: jest.fn(),
     trackException: jest.fn()
-  }
+  },
+  setup: function () { return this },
+  start: jest.fn()
 }
+
+export default appInsights
