2c4b717186d3957606f89053e7606088075f2221
Aaron Carroll <20041831+aaroncarroll82@users.noreply.github.com>
2025-07-18 14:21:52 +0100

    fix(AHWR-1108): Accessibility issues (#179)
    * fix(AHWR-1108): Accessibility issues
    
    * fix(AHWR-1108): Use patched MoJ library to resolve screen reader issue with sort arrows
    
    * fix(AHWR-1108): Wrap tables in plain divs (which makes them vertically wrap)

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ EXPOSE ${PORT} ${PORT_DEBUG}
 COPY --chown=node:node package*.json ./
 RUN npm install --ignore-scripts
 COPY --chown=node:node . .
+RUN npx patch-package
 RUN npm run build
 CMD [ "npm", "run", "start:watch" ]
 
--- app/frontend/src/entry.js
+++ app/frontend/src/entry.js
@@ -1,4 +1,5 @@
 import { initAll } from 'govuk-frontend'
+import { initAll as initMoj } from '@ministryofjustice/frontend'
 import "./css/application.scss";
 import "./css/document-list.scss";
 import "./css/devolved-nations.scss";
@@ -9,16 +10,9 @@ import "./css/organisation-logo.scss";
 import "./js/cookies.js";
 import "./js/handleDuplicateFormSubmissions.js";
 import jquery from "jquery";
-import moj from "@ministryofjustice/frontend";
 
 initAll()
+initMoj()
 
 window.$ = jquery;
 
-const $sortableTables = document.querySelectorAll(
-  '[data-module="moj-sortable-table"]',
-);
-moj.nodeListForEach(
-  $sortableTables,
-  (table) => new moj.SortableTable({ table }),
-);
