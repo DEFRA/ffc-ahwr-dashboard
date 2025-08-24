290f792af0546eb38ac27690a3306baaff9fb079
Rob Catton <robmcatton@gmail.com>
2025-08-24 18:12:10 +0100

    Increasing test coverage

---
+++ test/integration/narrow/routes/sign-out.test.js
@@ -0,0 +1,40 @@
+import { StatusCodes } from "http-status-codes";
+import { createServer } from "../../../../app/server.js";
+import { getToken, clearAllOfSession } from "../../../../app/session/index.js";
+import { clearAuthCookie } from "../../../../app/auth/cookie-auth/cookie-auth.js";
+import { claimServiceUri } from "../../../../app/config/routes.js";
+
+jest.mock("../../../../app/session/index.js");
+jest.mock("../../../../app/auth/cookie-auth/cookie-auth.js");
+
+describe("GET /sign-out handler", () => {
+  let server;
+
+  beforeAll(async () => {
+    server = await createServer();
+  });
+
+  test("get /sign-out", async () => {
+    getToken.mockResolvedValueOnce("access-token");
+
+    const res = await server.inject({
+      url: "/sign-out",
+      auth: {
+        credentials: {},
+        strategy: "cookie",
+      },
+    });
+  
+    expect(getToken).toHaveBeenCalled();
+    expect(clearAllOfSession).toHaveBeenCalled();
+    expect(clearAuthCookie).toHaveBeenCalled();
+    expect(res.statusCode).toBe(StatusCodes.MOVED_TEMPORARILY);
+
+    const url = new URL(res.headers.location);
+    const { searchParams } = url;
+
+    expect(url.origin).toBe("https://dcidmtest.b2clogin.com");
+    expect(url.pathname).toMatch(/\/oauth2\/v2\.0\/logout$/);
+    expect(searchParams.get("post_logout_redirect_uri")).toBe(claimServiceUri);
+  });
+});
