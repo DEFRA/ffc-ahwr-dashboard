407257709fe60a019a44df40a6eacab4ecb6add6
Rob Catton <robmcatton@gmail.com>
2025-09-26 14:13:09 +0100

    PR comments and general small refactors around session

--- test/unit/app/session/session.test.js
+++ test/unit/app/session/session.test.js
@@ -1,21 +1,17 @@
 import {
-  clear,
   clearAllOfSession,
   entries,
-  getApplication,
   getCustomer,
   getEndemicsClaim,
   getFarmerApplyData,
   getPkcecodes,
   getToken,
-  setApplication,
   setCustomer,
   setEndemicsClaim,
   setFarmerApplyData,
   setPkcecodes,
   setToken,
 } from "../../../../app/session/index.js";
-import { sendSessionEvent } from "../../../../app/event/send-session-event.js";
 
 jest.mock("../../../../app/event/send-session-event");
 
@@ -36,21 +32,6 @@ describe("session", () => {
     jest.resetAllMocks();
   });
 
-  describe("clear", () => {
-    test("yar.clear is called for all entries excluding tokens and pkcecodes", () => {
-      const request = { yar: yarMock };
-      clear(request);
-
-      const entriesToClear = Object.values(entries).filter(
-        (v) => v !== "tokens" && v !== "pkcecodes"
-      );
-      expect(yarMock.clear).toHaveBeenCalledTimes(entriesToClear.length);
-      entriesToClear.forEach((entryToClear) => {
-        expect(yarMock.clear).toHaveBeenCalledWith(entryToClear);
-      });
-    });
-  });
-
   describe("clearAllOfSession", () => {
     test("yar.clear is called for all entries (no exclusions)", () => {
       const request = { yar: yarMock };
@@ -62,64 +43,6 @@ describe("session", () => {
       entryKeyValuePairs.forEach(([key, value]) => {
         expect(yarMock.clear).toHaveBeenCalledWith(value);
       });
-    });
-  });
-
-  describe("Set", () => {
-    test("Send session event called with correct variables - no header", () => {
-      const request = {
-        yar: yarMock,
-        headers: {},
-        info: { remoteAddress: "123" },
-      };
-      setApplication(request, "test key", 123);
-      expect(yarMock.set).toHaveBeenCalledWith("application", {
-        "test key": 123,
-      });
-      expect(sendSessionEvent).toHaveBeenCalledWith(
-        undefined,
-        1,
-        "application",
-        "test key",
-        123,
-        "123"
-      );
-    });
-
-    test("Send session event called with correct variables - header", () => {
-      const request = {
-        yar: yarMock,
-        headers: { "x-forwarded-for": "1,2,3" },
-        info: { remoteAddress: "123" },
-      };
-      setApplication(request, "test key", 123);
-      expect(yarMock.set).toHaveBeenCalledWith("application", {
-        "test key": 123,
-      });
-      expect(sendSessionEvent).toHaveBeenCalledWith(
-        undefined,
-        1,
-        "application",
-        "test key",
-        123,
-        "1"
-      );
-    });
-  });
-
-  describe("Application", () => {
-    test("set called with correct variables", () => {
-      const request = { yar: yarMock, headers: { "x-forwarded-for": "1,2,3" } };
-      setApplication(request, "test key", "test value");
-      expect(yarMock.set).toHaveBeenCalledWith("application", {
-        "test key": "test value",
-      });
-    });
-
-    test("get called with correct variables", () => {
-      const request = { yar: yarMock, headers: { "x-forwarded-for": "1,2,3" } };
-      getApplication(request, "test key");
-      expect(yarMock.get).toHaveBeenCalledWith("application");
     });
   });
 
